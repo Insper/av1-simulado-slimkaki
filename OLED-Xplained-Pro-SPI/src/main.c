8462688ba74befc0a7abea37f78b9eb44db07663
slimkaki <rafaalmada98@gmail.com>
2020-04-06 16:28:43 -0300

    Iniciando botoes

--- OLED-Xplained-Pro-SPI/src/main.c
+++ OLED-Xplained-Pro-SPI/src/main.c
@@ -3,6 +3,78 @@
 #include "gfx_mono_ug_2832hsweg04.h"
 #include "gfx_mono_text.h"
 #include "sysfont.h"
+/*				CONFIGURANDO BOTOES				*/
+//	Botao 1 - OLED
+#define BUT1_PIO			PIOD
+#define BUT1_PIO_ID			16
+#define BUT1_PIO_IDX		28
+#define BUT1_PIO_IDX_MASK	(1u << BUT1_PIO_IDX)
+
+//	Botao 2 - OLED
+#define BUT2_PIO			PIOC
+#define BUT2_PIO_ID			12
+#define BUT2_PIO_IDX		31
+#define BUT2_PIO_IDX_MASK	(1u << BUT2_PIO_IDX)
+
+//	Botao 3 - OLED
+#define BUT3_PIO			PIOA
+#define BUT3_PIO_ID			10
+#define BUT3_PIO_IDX		19
+#define BUT3_PIO_IDX_MASK	(1u << BUT3_PIO_IDX)
+
+/*			Definindo as flags de botoes			*/
+volatile char but_flag1;
+volatile char but_flag2;
+volatile char but_flag3;
+
+/*			Funcoes de Callback				*/
+// Inicando as funcoes
+void but1_callback(void);
+void but2_callback(void);
+void but3_callback(void);
+
+// Funcoes
+
+void but1_callback(void) {
+	but_flag1 = 1;
+}
+
+void but2_callback(void) {
+	but_flag2 = 1;
+}
+
+void but3_callback(void) {
+	but_flag3 = 1;
+}
+
+/*			Funcao para inicializar botoes			*/
+void but_init() {
+	// Enable Peripheral Clock
+	pmc_enable_periph_clk(BUT1_PIO_ID);
+	pmc_enable_periph_clk(BUT2_PIO_ID);
+	pmc_enable_periph_clk(BUT3_PIO_ID);
+	
+	pio_configure(BUT1_PIO, PIO_INPUT, BUT1_PIO_IDX_MASK, PIO_PULLUP); // SUBIDA
+	pio_configure(BUT2_PIO, PIO_INPUT, BUT2_PIO_IDX_MASK, pio_pull_down()); // DESCIDA
+	pio_configure(BUT3_PIO, PIO_INPUT, BUT3_PIO_IDX_MASK, PIO_PULLUP); // SUBIDA
+	
+	pio_handler_set(BUT1_PIO, BUT1_PIO_ID, BUT1_PIO_IDX_MASK, PIO_IT_RISE_EDGE, but1_callback);
+	pio_handler_set(BUT2_PIO, BUT2_PIO_ID, BUT2_PIO_IDX_MASK, PIO_IT_RISE_EDGE, but2_callback);
+	pio_handler_set(BUT3_PIO, BUT3_PIO_ID, BUT3_PIO_IDX_MASK, PIO_IT_RISE_EDGE, but3_callback);
+	
+	pio_enable_interrupt(BUT1_PIO, BUT1_PIO_IDX_MASK);
+	pio_enable_interrupt(BUT2_PIO, BUT2_PIO_IDX_MASK);
+	pio_enable_interrupt(BUT3_PIO, BUT3_PIO_IDX_MASK);
+	
+	NVIC_EnableIRQ(BUT1_PIO_ID);
+	NVIC_SetPriority(BUT1_PIO_ID, 3); // BOTAO 1 - Prioridade 4
+	NVIC_EnableIRQ(BUT2_PIO_ID);
+	NVIC_SetPriority(BUT2_PIO_ID, 3); // BOTAO 2 - Prioridade 4
+	NVIC_EnableIRQ(BUT3_PIO_ID);
+	NVIC_SetPriority(BUT3_PIO_ID, 3); // BOTAO 3 -Prioridade 4
+}
+
+
 
 int main (void)
 {
@@ -15,7 +87,7 @@ int main (void)
   
   // Escreve na tela um circulo e um texto
 	gfx_mono_draw_filled_circle(20, 16, 16, GFX_PIXEL_SET, GFX_WHOLE);
-  gfx_mono_draw_string("mundo", 50,16, &sysfont);
+	gfx_mono_draw_string("mundo", 50,16, &sysfont);
 
   /* Insert application code here, after the board has been initialized. */
 	while(1) {
